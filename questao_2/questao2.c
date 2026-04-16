0fec234dac8dc82ed040b917c3d057c8c8abfe5c
Eduarda <zoospencer4@gmail.com>
2026-04-16 10:27:42 -0300

    questão 2 funcionando

--- questao_2/questao2.c
+++ questao_2/questao2.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct _Endereco Endereco;
-
-struct _Endereco {
+typedef struct {
     char logradouro[72];
     char bairro[72];
     char cidade[72];
@@ -9,87 +10,84 @@ struct _Endereco {
     char sigla[2];
     char cep[8];
     char lixo[2];
-};
+} Endereco;
 
 typedef struct {
     char cep[8];
-    long posicao;
+    long pos;
 } Indice;
 
-int comparaIndice(const void *a, const void *b) {
-    return strncmp(((Indice *)a)->cep, ((Indice *)b)->cep, 8);
+int compara(const void *e1, const void *e2){
+    return strncmp(((Indice*)e1)->cep, ((Indice*)e2)->cep, 8);
 }
-int buscaBinariaIndice(Indice *v, long qtd, char *cepBuscado) {
-    long ini = 0, fim = qtd - 1, meio;
-    int cmp;
 
-    while (ini <= fim) {
-        meio = (ini + fim) / 2;
-        cmp = strncmp(cepBuscado, v[meio].cep, 8);
-
-        if (cmp == 0) {
-            return meio;
-        }
-        else if (cmp < 0) {
-            fim = meio - 1;
-        }
-        else {
-            ini = meio + 1;
-        }
-    }
-    return -1;
-}
 int main(int argc, char *argv[]) {
-    FILE *fDados, *fIndice;
-    Endereco e;
-    Indice *indice;
-    long qtd, i, tam;
-    char cepBuscado[9];
-    int posEncontrada;
 
-    if (argc != 4) {
-        printf("Uso: %s <arquivo_dados.dat> <arquivo_indice.dat> <CEP>", argv[0]);
+    if(argc != 2){
+        fprintf(stderr, "USO: %s [CEP]\n", argv[0]);
         return 1;
     }
-    fDados = fopen(argv[1], "rb");
-    fseek(fDados, 0, SEEK_END);
-    tam = ftell(fDados);
-    qtd = tam / sizeof(Endereco);
-    rewind(fDados);
 
-    indice = (Indice *) malloc(qtd * sizeof(Indice));
+    char *proc = argv[1];
 
-    for (i = 0; i < qtd; i++) {
-        fread(&e, sizeof(Endereco), 1, fDados);
-        memcpy(indice[i].cep, e.cep, 8);
-        indice[i].posicao = i;
+    FILE *f = fopen("cep_ordenado.dat", "rb");
+    if(!f){
+        printf("Erro ao abrir arquivo\n");
+        return 1;
     }
-    qsort(indice, qtd, sizeof(Indice), comparaIndice);
-    fIndice = fopen(argv[2], "wb");
-    fwrite(indice, sizeof(Indice), qtd, fIndice);
-    fclose(fIndice);
-    strncpy(cepBuscado, argv[3], 8);
-    cepBuscado[8] = '\0';
-    posEncontrada = buscaBinariaIndice(indice, qtd, cepBuscado);
 
-    if (posEncontrada == -1) {
-        printf("CEP nao encontrado");
+    fseek(f,0,SEEK_END);
+    long total = ftell(f)/sizeof(Endereco);
+    rewind(f);
+
+    Indice *vet = malloc(total * sizeof(Indice));
+
+    for(long i = 0; i <total; i++){
+        Endereco e;
+        fread(&e, sizeof(Endereco), 1, f);
+
+        strncpy(vet[i].cep, e.cep, 8);
+        vet[i].pos = i;
     }
-    else {
-        printf("CEP encontrado no indice!");
-        printf("Posicao no arquivo original: %ld", indice[posEncontrada].posicao);
 
-        fseek(fDados, indice[posEncontrada].posicao * sizeof(Endereco), SEEK_SET);
-        fread(&e, sizeof(Endereco), 1, fDados);
+    qsort(vet,total,sizeof(Indice),compara);
 
-        printf("Logradouro: %.72s", e.logradouro);
-        printf("Bairro: %.72s", e.bairro);
-        printf("Cidade: %.72s", e.cidade);
-        printf("UF: %.72s", e.uf);
-        printf("Sigla: %.2s", e.sigla);
-        printf("CEP: %.8s", e.cep);
+    FILE *g = fopen("indice.dat", "wb");
+    fwrite(vet, sizeof(Indice), total, g);
+    fclose(g);
+
+    long inicio = 0;
+    long fim = total -1;
+
+    while(inicio <= fim){
+        long meio = (inicio + fim) / 2;
+
+        if(strncmp(proc, vet[meio].cep, 8) == 0){
+
+            long pos = vet[meio].pos;
+
+            fseek(f, pos*sizeof(Endereco), SEEK_SET);
+
+            Endereco e;
+            fread(&e,sizeof(Endereco),1,f);
+
+            printf("CEP encontrado. Info: %.72s, %.72s, %.72s, %.72s, %.2s,%.8s ",e.logradouro, e.bairro, e.cidade, e.uf, e.sigla, e.cep);
+
+            free(vet);
+            fclose(f);
+            return 0;
+        }
+        else if(strncmp(proc,vet[meio].cep, 8) < 0){
+            fim = meio - 1;
+        }
+        else{
+            inicio = meio + 1;
+        }
     }
-    free(indice);
-    fclose(fDados);
+
+    printf("Esse CEP não foi encontrado :(\n");
+
+    free(vet);
+    fclose(f);
     return 0;
 }
